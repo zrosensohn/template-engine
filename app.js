60d215fa1a46f96cab38702a80b451d83a857afc
ZROSENSOHN LAPTOP <zac@satprepct.com>
2020-02-06 08:32:08 -0500

    shifted questions into lib dir, added validation for some questions, and rewrote inquirer question flow to leverage the when function for conditional questions

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inq = require('inquirer');
+const questions = require('./lib/questions');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
@@ -8,120 +9,37 @@ let employeeArr = [];
 
 //Async Function for Adding Employees and creating classes
 async function employeeCLI() {
-    let employeeData = await employeeInput();
-    let employee = employeeData.employee;
-    let name = employeeData.name;
-    let id = employeeData.id;
-    let email = employeeData.email;
+    let employeeData = await inq.prompt(questions);
+    let { name, email, employee, phone, github, school, another} = employeeData;
+    let id = employeeArr.length + 1;
     
     if (employee === "Manager"){
-        let managerPhone = await managerInput();
-        let manager = new Manager(name, id, email, managerPhone.phone);
+        let manager = new Manager(name, id, email, phone);
         employeeArr.push(manager);
-        addEmployee();
 
     } else if (employee === "Engineer") {
-        let gitHub = await engineerInput();
-        let engineer = new Engineer(name, id, email, gitHub.github);
+        let engineer = new Engineer(name, id, email, github);
         employeeArr.push(engineer);
-        addEmployee();
 
     } else if (employee === "Intern"){
-        let school = await internInput();
-        let intern = new Intern(name, id, email, school.school);
+        let intern = new Intern(name, id, email, school);
         employeeArr.push(intern);
-        addEmployee();
     }
-}
 
-//Function for baseline data for all employee types
-function employeeInput() {
-    return inq.prompt([
-        {
-            type: "input",
-            message: "Enter Employee Name",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Enter Employee ID",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "Enter Email",
-            name: "email"
-        },
-        {
-            type: "list",
-            message: "Select Title",
-            choices: ["Manager", "Engineer", "Intern"],
-            name: "employee"
-        }
-    ]);
-}
-
-//function for managaer specific data
-function managerInput() {
-    return inq.prompt([
-        {
-            type: "input",
-            message: "Enter Manager Phone Number",
-            name: "phone"
-        }
-    ]);
-}
-
-//function for Engineer specific data
-function engineerInput() {
-    return inq.prompt([
-        {
-            type: "input",
-            message: "Enter GitHub Username",
-            name: "github"
-        }
-    ]);
-}
-
-//function for Intern specific data
-function internInput() {
-    return inq.prompt([
-        {
-            type: "input",
-            message: "Enter Intern's School",
-            name: "school"
-        }
-    ]);
-}
-
-//Function to continue CLI for adding another epmployee
-function addEmployee() {
-    inq.prompt([
-        {
-            type: "list",
-            message: "Add Another Employee?",
-            choices: ["Yes", "No"],
-            name: "continue"
-        }
-    ]).then(response => {
-        switch(response.continue){
-            case "Yes":
-                employeeCLI();
-                break;
-            default:
-                console.log(employeeArr);
-                employeeArr.forEach(item => {
-                    if (item instanceof Manager) {
-                        console.log(item.getId());
-                        console.log(item.getName());
-                        console.log(item.getRole());
-                        console.log(item.getEmail());
-                        console.log(item.getOfficeNumber());
-                    }
-                });
-                break;
-        }
-    });
+    if(another){
+        employeeCLI();
+    } else {
+        console.log(employeeArr);
+        employeeArr.forEach(item => {
+            if (item instanceof Manager) {
+                console.log(item.getId());
+                console.log(item.getName());
+                console.log(item.getRole());
+                console.log(item.getEmail());
+                console.log(item.getOfficeNumber());
+            }
+        });
+    }
 }
 
 //Kicking things off for testing
